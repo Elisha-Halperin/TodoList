6cca7aa2cabe2cfeb1d7b33b5d0ca767394a3ee1
Elisha-Halperin <elishahalperin@yahoo.com>
2015-04-01 15:28:06 +0300

    V1

--- app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -78,11 +78,6 @@ public class TodoListManagerActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
         if (id == R.id.menuItemAdd) {
             String tmp = ((EditText)findViewById(R.id.editText)).getText().toString();
             if (tmp != null && !tmp.isEmpty()) {
