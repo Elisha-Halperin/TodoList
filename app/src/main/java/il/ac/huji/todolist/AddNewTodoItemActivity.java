1e0dff9aa3d5b1ebfa2a29365317f3ea8060955b
Elisha-Halperin <elishahalperin@yahoo.com>
2015-04-15 04:37:23 +0300

    V2

---
+++ app/src/main/java/il/ac/huji/todolist/AddNewTodoItemActivity.java
@@ -0,0 +1,69 @@
+package il.ac.huji.todolist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+
+
+public class AddNewTodoItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_todo_item);
+
+        final EditText edTxt = (EditText)findViewById(R.id.edtNewItem);
+        final DatePicker date = (DatePicker)findViewById(R.id.datePicker);
+        findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_CANCELED);
+                finish();
+            }
+        });
+        findViewById(R.id.btnOK).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent result = new Intent();
+                result.putExtra("title", edTxt.getText().toString());
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(date.getYear(), date.getMonth(), date.getDayOfMonth());
+                result.putExtra("dueDate", calendar.getTime());
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_new_todo_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -2,7 +2,9 @@ package il.ac.huji.todolist;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
@@ -13,37 +15,55 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
 public class TodoListManagerActivity extends ActionBarActivity {
 
+    protected String txt;
     protected int current;
     protected ListView ls;
     protected Dialog dialog;
-    protected ArrayAdapter<String> adapter;
-    protected ArrayList<String> list = new ArrayList<>();
+    protected ArrayAdapter adapter;
+    protected ArrayList<ListObj> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
 
-        ls = (ListView)findViewById(R.id.todoList);
-        ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ls = (ListView)findViewById(R.id.lstTodoItems);
+        ls.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
             @Override
-            public void onItemClick(final AdapterView<?> parent, View view, int position, long id) {
+            public boolean onItemLongClick(final AdapterView<?> parent, View view, int position, long id) {
+                Button call;
                 dialog = new Dialog(parent.getContext());
                 LayoutInflater inflater = (LayoutInflater)
                         parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 View bodyView = inflater.inflate(R.layout.dialog_body, null);
-                ((TextView)bodyView.findViewById(R.id.textViewTitle)).setText(list.get(position));
+                dialog.setTitle(list.get(position).txt);
+                txt = list.get(position).txt;
+                if (txt != null && txt.toLowerCase().startsWith("call ")) {
+                    call = (Button) bodyView.findViewById(R.id.menuItemCall);
+                    call.setText(list.get(position).txt);
+                    call.setVisibility(View.VISIBLE);
+                    call.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                            startActivity(new Intent(Intent.ACTION_DIAL,
+                                    Uri.parse("tel:" + txt.split(" ")[1])));
+                        }
+                    });
+                }
                 Button btn = (Button)bodyView.findViewById(R.id.menuItemDelete);
                 dialog.setContentView(bodyView);
                 current = position;
@@ -57,6 +77,7 @@ public class TodoListManagerActivity extends ActionBarActivity {
                     }
                 });
                 dialog.show();
+                return true;
             }
         });
         adapter = new MyAdapter(this, android.R.layout.simple_list_item_1, list);
@@ -77,22 +98,23 @@ public class TodoListManagerActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         if (id == R.id.menuItemAdd) {
-            String tmp = ((EditText)findViewById(R.id.editText)).getText().toString();
-            if (tmp != null && !tmp.isEmpty()) {
-                list.add(tmp);
-                adapter.notifyDataSetChanged();
-                ((EditText)findViewById(R.id.editText)).getText().clear();
-            }
+            startActivityForResult(new Intent(this, AddNewTodoItemActivity.class), 0);
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    private class MyAdapter extends ArrayAdapter<String> {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            list.add(new ListObj(data.getStringExtra("title"), (Date) data.getSerializableExtra("dueDate")));
+            adapter.notifyDataSetChanged();
+        }
+    }
 
-        public MyAdapter(Context context, int textViewResourceId, List<String> objects) {
+    private class MyAdapter extends ArrayAdapter<ListObj> {
+
+        public MyAdapter(Context context, int textViewResourceId, List<ListObj> objects) {
             super(context, textViewResourceId, objects);
         }
 
@@ -101,11 +123,32 @@ public class TodoListManagerActivity extends ActionBarActivity {
             if (convertView == null) {
                 convertView = LayoutInflater.from(getContext()).inflate(R.layout.row, parent, false);
             }
-            TextView tx = (TextView) convertView.findViewById(R.id.lstTodoItems);
-            tx.setText(getItem(position));
-            tx.setTextColor(Color.WHITE);
-            tx.setBackgroundColor(position % 2 == 0 ? Color.RED : Color.BLUE);
+            Date dt = getItem(position).date;
+            String tx = getItem(position).txt;
+            TextView text = (TextView) convertView.findViewById(R.id.txtTodoTitle);
+            TextView date = (TextView) convertView.findViewById(R.id.txtTodoDueDate);
+            text.setText(tx != null ? tx : "");
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+            String dateString = sdf.format(dt);
+            date.setText(dt != null ? dateString : "No due date");
+            text.setTextColor(Color.BLACK);
+            date.setTextColor(Color.BLACK);
+            if (dt != null && dt.before(new Date())) {
+                text.setTextColor(Color.RED);
+                date.setTextColor(Color.RED);
+            }
             return convertView;
+        }
+    }
+
+    private class ListObj {
+
+        public String txt;
+        public Date date;
+
+        public ListObj(String str, Date dt) {
+            this.txt = str;
+            this.date = dt;
         }
     }
 }
